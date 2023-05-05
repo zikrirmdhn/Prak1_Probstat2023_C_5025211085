b61cd52d0748c37fa86ae88dab6aab3b035e8f83
zikrirmdhn <97161145+zikrirmdhn@users.noreply.github.com>
2023-05-05 23:29:04 +0700

    Add files via upload

---
+++ prak1no2.R
@@ -0,0 +1,29 @@
+# tingkat kejadian kematian dalam 20 tahun
+l <- 1.8
+
+#A. Pendistribusian banyak kematian karena kanker tulang
+Dist <- dpois(0:20, l)
+print(Dist)
+
+#B. Probabilitas 4 kematian akibat kanker tulang
+
+#C. Probabilitas paling banyak 4 kematian akibat kanker tulang
+prob <- ppois(4, l)
+print(prob)
+
+#D. Probabilitas lebih dari 4 kematian akibat kanker tulang
+prob <- 1 - ppois(4, l)
+print(prob)
+
+#E. Nilai harapan dan simpangan baku banyak kematian
+mean <- l
+sd <- sqrt(l)
+
+#F. Histogram pendistribusian banyak kematian
+
+
+#G. Simulasi
+
+
+#H. Penjelasan hasil simulasi
+
